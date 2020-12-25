f622474d22086d4834464ee7edc24692b256c426
chelsi-001011 <57683338+chelsi-001011@users.noreply.github.com>
2020-12-25 16:10:02 +0530

    Day 6

--- 20.cpp
+++ 20.cpp
@@ -28,3 +28,59 @@ int main() {
     return 0;
 }
 //2. without using built-in function
+// Next Permutation
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+void swap(int *a, int *b)
+{
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        //Next Permutation
+        int i = 0, j = 0;
+        for (i = n - 1; i >= 0; --i)
+        {
+            if (a[i] > a[i - 1])
+                break;
+        }
+
+        if (i == 0)
+        {
+            sort(a, a + n);
+        } else {
+            int x = a[i - 1], smallest = i;
+            for (j = i + 1; j < n; j++)
+                if (a[j] > x && a[j] < a[smallest])
+                    smallest = j;
+
+            swap(&a[smallest], &a[i - 1]);
+
+            sort(a + i, a + n);
+        }
+
+
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
