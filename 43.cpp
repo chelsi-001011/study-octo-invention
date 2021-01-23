d82994a3c4156e3fd99546ee6536d9216bd9530a
Chelsi Rawat <chelzee.rawat@yahoo.in>
2021-01-23 14:50:09 +0530

    Day 12

---
+++ 43.cpp
@@ -0,0 +1,35 @@
+
+// function to find longest common subsequence
+
+int lcs(int x, int y, string s1, string s2){
+    
+    int dp[x+1][y+1];
+   
+    for(int i=0;i<=x;i++)
+    {
+        for (int j=0;j<=y;j++)
+        {
+            dp[i][j]=0;
+        }
+    }
+    
+    for(int i=0;i<=x;i++)
+    {
+        for (int j=0;j<=y;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j]=0;
+                continue;
+            }
+            else if(s1[i-1]==s2[j-1])
+            {
+                dp[i][j]= 1+ dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    return dp[x][y];
+}
